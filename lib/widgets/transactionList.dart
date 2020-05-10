e0948f0ae7f41c32ce50122471e1f79c8a1cc0db
Gabriel Joshua Paet <gjvpaet@gmail.com>
2020-05-10 21:48:14 +0800

    Added const to some widgets and made the constructor of chartBar widget a const constructor.

--- lib/widgets/transactionList.dart
+++ lib/widgets/transactionList.dart
@@ -22,7 +22,7 @@ class TransactionList extends StatelessWidget {
                     'No transactions added yet!',
                     style: Theme.of(context).textTheme.headline6,
                   ),
-                  SizedBox(
+                  const SizedBox(
                     height: 20,
                   ),
                   Container(
@@ -40,7 +40,7 @@ class TransactionList extends StatelessWidget {
             itemBuilder: (ctx, i) {
               return Card(
                 elevation: 5,
-                margin: EdgeInsets.symmetric(
+                margin: const EdgeInsets.symmetric(
                   vertical: 8,
                   horizontal: 5,
                 ),
@@ -48,7 +48,7 @@ class TransactionList extends StatelessWidget {
                   leading: CircleAvatar(
                     radius: 30,
                     child: Padding(
-                      padding: EdgeInsets.all(6),
+                      padding: const EdgeInsets.all(6),
                       child: FittedBox(
                         child: Text(
                           '\$${transactions[i].amount}',
@@ -68,16 +68,18 @@ class TransactionList extends StatelessWidget {
                   trailing: mediaQuery.size.width > 460
                       ? FlatButton.icon(
                           onPressed: () => deleteTx(transactions[i].id),
-                          icon: Icon(
+                          icon: const Icon(
                             Icons.delete,
                           ),
-                          label: Text(
+                          label: const Text(
                             'Delete',
                           ),
                           textColor: Theme.of(context).errorColor,
                         )
                       : IconButton(
-                          icon: Icon(Icons.delete),
+                          icon: const Icon(
+                            Icons.delete,
+                          ),
                           color: Theme.of(context).errorColor,
                           onPressed: () => deleteTx(
                             transactions[i].id,
